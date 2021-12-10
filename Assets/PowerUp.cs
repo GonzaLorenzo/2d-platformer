726f6b3b6c6744385a6965e1c2e234d0db9d14f9
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2021-12-10 06:07:46 -0300

    Decorator pt1

---
+++ Assets/PowerUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PowerUp : IController
+{
+    protected IController _controller;
+
+    public PowerUp(IController controller)
+    {
+        _controller = controller;
+    }
+    public void OnFixedUpdate()
+    {
+        
+    }
+
+    public void OnUpdate()
+    {
+
+    }
+}
---
+++ Assets/SpeedPU.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedPU : PowerUp
+{
+    public SpeedPU(IController controller) : base(controller)
+    {
+
+    }
+
+    //public override string O
+}
